3334a323447b2619c515f3c3f746d98e79990612
Alonso21HB <fhinostroza@autonoma.edu.pe>
2025-02-04 16:26:46 -0500

    proyecto apirest

---
+++ app.py
@@ -0,0 +1,58 @@
+from flask import Flask
+from flask import jsonify
+from flask import request
+
+app = Flask(__name__)
+
+
+class Tarea:
+    def __init__(self, id: int, titulo: str, descripcion: str):
+        self.id = id
+        self.titulo  = titulo
+        self.descripcion = descripcion
+    
+    def diccionario(self):
+        return{
+            "id":self.id,
+            "titulo": self.titulo,
+            "descripcion":self.descripcion
+        }
+    
+tareas = [
+    Tarea(1, "Aprender Flask", "Estudiar los fundamentos de Flask"),
+    Tarea(2, "Leer un libro","Leer un libro de desarrollo web")
+]
+
+@app.route('/')
+def home():
+    return "Bienvenido a mi primera api en flask"
+
+@app.route('/tareas', methods=['GET'])
+def obtener_tareas():
+    return jsonify([tarea.diccionario() for tarea in tareas])
+
+@app.route('/tareas', methods=['POST'])
+def agregar_tarea():
+    nueva_tarea = Tarea(id=request.json['id'],
+                        titulo = request.json['titulo'],
+                        descripcion=request.json['descripcion'])
+    tareas.append(nueva_tarea)
+    return jsonify(nueva_tarea.diccionario()), 201
+
+
+@app.route('/tareas/<int:id>', methods=['PUT'])
+def actualizar_tarea(id):
+    tarea = next((t for t in tareas if t.id == id), None)
+    if tarea is not None:
+        tarea.titulo = request.json.get('titulo', tarea.titulo)
+        tarea.descripcion = request.json.get('descripcion', tarea.descripcion)
+        return jsonify(tarea.diccionario())
+    return jsonify({"error": "tarea no encontrada"}), 404
+
+@app.route('/tareas/<int:id>', methods=['DELETE'])
+def eliminar_tarea(id):
+    global tareas
+    tareas = [t for t in tareas if t.id != id]
+    return jsonify({'resultado': True})
+if __name__ == '__main__':
+    app.run(debug=True)
